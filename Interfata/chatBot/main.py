44f6ebbbe31db9a9b0eb4b488ef6100778c52e46
Irinel Manolache <Irinel Manolache>
2017-12-17 15:11:42 +0200

    Robert,Vladut,Irinel

--- Interfata/chatBot/main.py
+++ Interfata/chatBot/main.py
@@ -3,11 +3,21 @@ from flask import request
 import requests
 import  validators
 app = Flask(__name__)
+lista = [("inima","este un organ al corpului","../static/john"),("inima","este un organ special")]
+
 
 
 @app.route("/")
-def profile():
-    return render_template("mainPage.html")
+def index():
+    return render_template("mainPage.html", dialogue=list())
+
+@app.route("/<dialogue>", methods = ["POST","GET"])
+def discutie(dialogue = None):
+    query = request.form["querry"]
+    print(query) #aici se afla intrebarea pusa pe site
+    #as vrea ca in lista sa fi pusa o lista de tuple de forma: (nume topic, text, cale_imagine)
+    #cale_imagine o sa fie ca calea spre imagine daca exista, None caz contrar
+    return render_template("mainPage.html", dialogue=lista)
 
 
 if __name__=="__main__":
