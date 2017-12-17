3dc8ba85494414e106e7762be2f0556b91903873
AndreiPi <aandrei.pi@gmail.com>
2017-12-17 02:59:34 +0200

    CompatibilityFixies on tree structure

--- Adnotarea/structarborescenta.py
+++ Adnotarea/structarborescenta.py
@@ -1,8 +1,19 @@
 import re
+import docx
+import os
 
+def read_from_file(path):
+    try:
+        text = ''
+        doc = docx.Document(path)
+        for line in doc.paragraphs:
+            text += line.text
+        return text
+    except IOError:
+        print("Couldn't open the file!")
 
 def arbore(fisier):
-    adnotare = open(fisier, "rt")
+    adnotare = open(fisier, "rt",encoding='utf8')
     text = adnotare.read()
     result = re.findall("<node parent=\"\s*\w+.*\">\s*\w+.*</node>", text)
     #print(result)
@@ -23,7 +34,9 @@ def arbore(fisier):
         else:
             arb.update({cheie: [value.strip()]})
     return arb
-
-
-#print(arbore("adnotareTest.txt"))
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# path = os.path.join(dir_path,"3.VASCULAR2012_corr.docx")
+# print(path)
+# text=read_from_file(path)
+print(arbore("adnotari.txt"))
 
