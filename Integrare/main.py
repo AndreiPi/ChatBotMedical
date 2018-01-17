efdfa3d6e98508043edb25224394961ff0de2c99
Alexandru Corfu <corfu.alexandru01@gmail.com>
2018-01-17 12:06:56 +0200

    Added a history on chat

--- Integrare/main.py
+++ Integrare/main.py
@@ -6,42 +6,64 @@ import validators
 import FindDefinition
 import WikiTextImage
 import integrareAiml as ia
+import datetime
 
 app = Flask(__name__)
-lista = [("inima", "este un organ al corpului", "../static/john"), ("inima", "este un organ special")]
+history = []
 
+def getLatestInputs(history):
+    latestInputs = []
+    for i in history[-3:]:
+        latestInputs.append(i)
+    return latestInputs
 
 @app.route("/")
 def index():
-    return render_template("finalPage.html", dialogue=list())
+    return render_template("finalPage.html", dialogue=history)
 
 
 @app.route("/<dialogue>", methods=["POST", "GET"])
 def discutie(dialogue=None):
+    global history
     query = request.form["querry"]
     print(query)
-    print("1 + ", query)  # aici se afla intrebarea pusa pe site
-    sub,prop=FindDefinition.get_prop(query,0)
-    ok, theme = WikiTextImage.extractContent(query)
-    # lista=[(sub,prop,None)]
+    now = datetime.datetime.now()
+
+    localtime = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " - " + str(now.day) + "/" + str(now.month) + "/" + str(now.year)
+
+    #currentInput = ("[" + localtime + "] User", query)
+
+    #Introducere in dictionar a formatului corect
+    #history.append(currentInput)
+
+    nrOfWords = query.split(" ")
+    #
+    # if len(nrOfWords) > 2:
+    #     sub, prop = FindDefinition.get_prop(query, 0)
+    #     if len(prop) > 0:
+    #         history.append(("[" + localtime + "] Bot", prop, None))
+    #     else:
+    #         history.append(("[" + localtime + "] Bot", "Nu am gasit prin textele adnotate!", None))
+    # else:
+
+    ok = WikiTextImage.extractContent(query)
+    theme = query
+
     if ok:
         find_An_Img = False
         dir_path = os.path.dirname(os.path.realpath(__file__))
         dir_path = os.path.join(dir_path,"static")
         path_dir = os.path.join(dir_path,theme)
-        print(path_dir)
-        print(os.path.isdir(path_dir))
         img_name = ""
         find_text = False
         txt_name = ""
         images=[]
         nr_images=0
         for fille in os.listdir(path_dir):
-            if fille.endswith(".png") or fille.endswith(".jpg") or fille.endswith("jpeg"):
+            if fille.endswith(".png") or fille.endswith(".jpg") or fille.endswith(".jpeg"):
                 if not find_An_Img:
                     img_name = fille
                     find_An_Img = True
-                    print(fille, "\n")
                 else:
                     images.append("../static/" +theme+"/"+fille)
                     nr_images=nr_images+1
@@ -49,7 +71,6 @@ def discutie(dialogue=None):
                 if not find_text:
                     txt_name = fille
                     find_text = True
-                    print(fille, "\n")
         if find_An_Img:
             path_img = "../static/" +theme+"/"+img_name
         else:
@@ -64,15 +85,16 @@ def discutie(dialogue=None):
             for a_line in f:
                 lines.append((a_line))
                 nrlines+=1
-            print(lines)
-            lista = [(query,read_data, path_img,lines,images,range(1,nr_images))]
-    else:
+            history += [("[" + localtime + "] Bot", read_data, path_img,lines,images,range(0,nr_images))]
+
+   # else:
         #lista = [(query, query + query, None)]
-        lista = [(sub, prop, None)]
+       # lista = [(sub, prop, None)]
     # as vrea ca in lista sa fi pusa o lista de tuple de forma: (nume topic, text, cale_imagine)
     # cale_11imagine o sa fie ca calea spre imagine daca exista, None caz contrar
     #return render_template("mainPage.html", dialogue=lista)
-    return render_template("secondPage.html", dialogue=lista)
+    #getLatestInputs(history)
+    return render_template("finalPage.html", dialogue=getLatestInputs(history))
 
 
 if __name__ == "__main__":
