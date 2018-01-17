0cbef604f88676d12d5eecac3ec249ff774f3dce
Alexandru Corfu <corfu.alexandru01@gmail.com>
2018-01-18 00:08:05 +0200

    Forgot to modify main's funct.

--- Integrare/main.py
+++ Integrare/main.py
@@ -21,8 +21,9 @@ def discutie(dialogue=None):
     query = request.form["querry"]
     print(query)
     print("1 + ", query)  # aici se afla intrebarea pusa pe site
-    sub,prop=FindDefinition.get_prop(query,0)
-    ok, theme = WikiTextImage.extractContent(query)
+   # sub,prop=FindDefinition.get_prop(query,0)
+    ok = WikiTextImage.extractContent(query)
+    theme = query
     # lista=[(sub,prop,None)]
     if ok:
         find_An_Img = False
@@ -67,8 +68,8 @@ def discutie(dialogue=None):
             print(lines)
             lista = [(query,read_data, path_img,lines,images,range(1,nr_images))]
     else:
-        #lista = [(query, query + query, None)]
-        lista = [(sub, prop, None)]
+        lista = [(query, query + query, None)]
+        #lista = [(sub, prop, None)]
     # as vrea ca in lista sa fi pusa o lista de tuple de forma: (nume topic, text, cale_imagine)
     # cale_11imagine o sa fie ca calea spre imagine daca exista, None caz contrar
     #return render_template("mainPage.html", dialogue=lista)
